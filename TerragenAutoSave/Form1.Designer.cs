4c4337634946c4a8aac5b7631140746a4f5a8013
Matt Mulholland <mulwooky@googlemail.com>
2017-07-30 18:12:49 +0100

    Terragen Auto Save
    Launches TGD.exe and auto saves projects every 20 seconds

---
+++ TerragenAutoSave/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TerragenAutoSave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.autoSaveOnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoSaveOffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Terragen Auto Save";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.autoSaveOnToolStripMenuItem,
+            this.autoSaveOffToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(148, 70);
+            // 
+            // autoSaveOnToolStripMenuItem
+            // 
+            this.autoSaveOnToolStripMenuItem.Checked = true;
+            this.autoSaveOnToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.autoSaveOnToolStripMenuItem.Name = "autoSaveOnToolStripMenuItem";
+            this.autoSaveOnToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.autoSaveOnToolStripMenuItem.Text = "Auto Save On";
+            this.autoSaveOnToolStripMenuItem.Click += new System.EventHandler(this.autoSaveOnToolStripMenuItem_Click);
+            // 
+            // autoSaveOffToolStripMenuItem
+            // 
+            this.autoSaveOffToolStripMenuItem.Name = "autoSaveOffToolStripMenuItem";
+            this.autoSaveOffToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.autoSaveOffToolStripMenuItem.Text = "Auto Save Off";
+            this.autoSaveOffToolStripMenuItem.Click += new System.EventHandler(this.autoSaveOffToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "*.exe";
+            this.openFileDialog1.InitialDirectory = "c:\\";
+            this.openFileDialog1.Title = "Browse to Terrage TGD.exe";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Terragen Auto Saver";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem autoSaveOnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autoSaveOffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ TerragenAutoSave/Form1.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerragenAutoSave
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindowAsync(HandleRef hWnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr WindowHandle);
+        public const int SW_Max = 3;
+        string file = null;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            RegistryKey localKey;
+            if (Environment.Is64BitOperatingSystem)
+                localKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+            else
+                localKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry32);
+            try
+            {
+                file = localKey.OpenSubKey(@"Software\TerragenAutoSave").GetValue("TGDLocation").ToString();
+            }
+            catch
+            { 
+                DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+                if (result == DialogResult.OK) // Test result.
+                {
+                    file = openFileDialog1.FileName;
+                    RegistryKey key = Registry.CurrentUser.OpenSubKey("Software", true);
+                    key.CreateSubKey("TerragenAutoSave");
+                    key = key.OpenSubKey("TerragenAutoSave", true);
+                    key.SetValue("TGDLocation", file);
+                }
+            }
+            
+            timer1.Enabled = true;
+            timer1.Start();
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = file.ToString()
+                }
+            };
+            process.Start();
+            Thread.Sleep(5000);
+            FocusStartProcess("tgd");
+            localKey.Dispose();
+            
+        }
+
+        private void FocusProcess(string procName)
+        {
+            Process[] objProcesses = System.Diagnostics.Process.GetProcessesByName(procName);
+            if (objProcesses.Length > 0)
+            {
+                IntPtr hWnd = IntPtr.Zero;
+                hWnd = objProcesses[0].MainWindowHandle;
+                //ShowWindowAsync(new HandleRef(null, hWnd), SW_Max);
+                SetForegroundWindow(objProcesses[0].MainWindowHandle);
+                SendKeys.Send("^s");
+            }
+            else
+            {
+                notifyIcon1.Visible = false;
+                notifyIcon1.Icon = null;
+                notifyIcon1.Dispose();
+                Application.Exit();
+            }
+        }
+
+        private void FocusStartProcess(string procName)
+        {
+            Process[] objProcesses = System.Diagnostics.Process.GetProcessesByName(procName);
+            if (objProcesses.Length > 0)
+            {
+                IntPtr hWnd = IntPtr.Zero;
+                hWnd = objProcesses[0].MainWindowHandle;
+                ShowWindowAsync(new HandleRef(null, hWnd), SW_Max);
+                SetForegroundWindow(objProcesses[0].MainWindowHandle);
+            }
+            else
+            {
+                notifyIcon1.Visible = false;
+                notifyIcon1.Icon = null;
+                notifyIcon1.Dispose();
+                Application.Exit();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            FocusProcess("tgd");
+        }
+
+        private void autoSaveOnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            autoSaveOnToolStripMenuItem.Checked = true;
+            autoSaveOffToolStripMenuItem.Checked = false;
+            timer1.Enabled = true;
+            timer1.Stop();
+            timer1.Start();
+        }
+
+        private void autoSaveOffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            autoSaveOnToolStripMenuItem.Checked = false;
+            autoSaveOffToolStripMenuItem.Checked = true;
+            timer1.Enabled = false;
+            timer1.Stop();
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            notifyIcon1.Icon = null;
+            notifyIcon1.Dispose();
+            Application.Exit();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Process[] objProcesses = System.Diagnostics.Process.GetProcessesByName("TGD");
+            if (objProcesses.Length > 0)
+            {
+            }
+            else
+            {
+                notifyIcon1.Visible = false;
+                notifyIcon1.Icon = null;
+                notifyIcon1.Dispose();
+                Application.Exit();
+            }
+        }
+    }
+}
